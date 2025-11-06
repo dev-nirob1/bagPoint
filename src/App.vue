bff1e62df35a4b02f31d4a5bc13fe051b683b3cd
dev-nirob1 <dev.nirob1@gmail.com>
2025-11-06 21:29:09 +0600

    hero section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import FooterSection from './components/sections/FooterSection.vue';
 import NavbarSection from './components/sections/NavbarSection.vue';
+import HeroSection from './zems/Front/Components/Section/HeroSection.vue';
 
 </script>
 
 <template>
   <NavbarSection/>
+  <HeroSection/>
   <FooterSection/>
 </template>
 
--- src/zems/Front/Components/Section/HeroSection.vue
+++ src/zems/Front/Components/Section/HeroSection.vue
@@ -1,3 +1,47 @@
 <template>
-  hero section
+  <section class="hero relative">
+    <div class="medium-2 gap-2 align-center container">
+      <div class="content">
+        <HeroTitle>Carry Smart. Look Sharp.
+        </HeroTitle>
+        <BaseParagraph>BagPoint school backpacks are designed for students who want style, comfort, and durability.
+          Organize your books, laptop, and essentials with ease — wherever you go.</BaseParagraph>
+        <div class="flex gap-1">
+          <BaseButton class="bg-primary">Learn More</BaseButton>
+          <BaseButton class="bg-dark">Buy Now</BaseButton>
+        </div>
+      </div>
+
+      <div class="image">
+        <BaseImage image="/hero.jpg"></BaseImage>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.hero {
+  min-height: 100vh;
+  color: var(--white-color);
+}
+
+.hero::before,
+.hero::after {
+  content: '';
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  top: 0;
+  z-index: -1;
+}
+
+.hero::before {
+  left: 0;
+  background: var(--secondary-color);
+}
+
+.hero::after {
+  right: 0;
+  background: var(--white-color);
+}
+</style>
